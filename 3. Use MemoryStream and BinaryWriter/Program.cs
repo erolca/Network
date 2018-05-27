7223962cfc6f1d5c25b4c451adf8e52b54106f8e
Erolca <erol.calisgan@gmail.com>
2018-05-27 21:39:43 +0300

    Network ( MemoryStream tutorial )

--- 3. Use MemoryStream and BinaryWriter/Program.cs
+++ 3. Use MemoryStream and BinaryWriter/Program.cs
@@ -5,6 +5,9 @@ using System.IO;
 
 class Test
 {
+
+    const string fileName = "AppSettings.dat";
+
     // Create a byte array from a decimal.
     public static byte[] DecimalToByteArray(decimal src)
     {
@@ -12,7 +15,7 @@ class Test
         {
             using (BinaryWriter writer = new BinaryWriter(stream))
             {
-                writer.Write(src);
+                writer.Write(src);// write to in memorystream
                 return stream.ToArray();
             }
         }
@@ -22,5 +25,45 @@ class Test
         byte[] b = DecimalToByteArray(285998345545.563846696m);
         // Convert a decimal to a byte array and display.
         Console.WriteLine(BitConverter.ToString(b));
+
+
+      
+        WriteDefaultValues();
+        DisplayValues();
+    }
+
+    public static void WriteDefaultValues()
+    {
+        using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+        {
+            writer.Write(1.250F);
+            writer.Write(@"c:\Temp");
+            writer.Write(10);
+            writer.Write(true);
+        }
+    }
+
+    public static void DisplayValues()
+    {
+        float aspectRatio;
+        string tempDirectory;
+        int autoSaveTime;
+        bool showStatusBar;
+
+        if (File.Exists(fileName))
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                aspectRatio = reader.ReadSingle();
+                tempDirectory = reader.ReadString();
+                autoSaveTime = reader.ReadInt32();
+                showStatusBar = reader.ReadBoolean();
+            }
+
+            Console.WriteLine("Aspect ratio set to: " + aspectRatio);
+            Console.WriteLine("Temp directory is: " + tempDirectory);
+            Console.WriteLine("Auto save time set to: " + autoSaveTime);
+            Console.WriteLine("Show status bar: " + showStatusBar);
+        }
     }
 }
---
+++ 4. Use MemoryStream and BinaryReader to convert Byte array to decimal/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+//Use MemoryStream and BinaryReader to convert Byte array to decimal
+
+class Test
+{
+    public static byte[] DecimalToByteArray(decimal src)
+    {
+        using (MemoryStream stream = new MemoryStream())
+        {
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                writer.Write(src);
+                return stream.ToArray();
+            }
+        }
+    }
+
+    public static decimal ByteArrayToDecimal(byte[] src)
+    {
+        using (MemoryStream stream = new MemoryStream(src))
+        {
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                return reader.ReadDecimal();
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        byte[] b = BitConverter.GetBytes(true);
+      
+       
+
+        // Convert a decimal to a byte array and display.
+        b = DecimalToByteArray(12345678987654321.563846696m);
+        Console.WriteLine(BitConverter.ToString(b));
+
+        // Convert a byte array to a decimal and display.
+        Console.WriteLine(ByteArrayToDecimal(b));
+
+
+        /**/
+        byte[] b1 = BitConverter.GetBytes(255);
+        byte[] b2 = BitConverter.GetBytes(44.23);
+        byte[] b3 = BitConverter.GetBytes(0x25FF63);
+
+    }
+}
