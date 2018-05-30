dd4f9dc1932d8de9bc51ff170a1a65bf492a8d91
erolcalisgan <erolcalisgan@hotmail.com>
2018-05-30 16:34:50 +0300

    Network ..Uri

--- 33.3.3. Use a regular expression to extract all fully/Program.cs
+++ 33.3.3. Use a regular expression to extract all fully/Program.cs
@@ -28,6 +28,19 @@ class MainClass
                 Console.WriteLine(file);
             }
         }
+
+
+        client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+        Stream data = client.OpenRead("http://www.java2s.com");
+        StreamReader reader = new StreamReader(data);
+        string s = reader.ReadToEnd();
+        Console.WriteLine(s);
+        data.Close();
+        reader.Close();
+
+
+
     }
 }
 //Downloading http://www.java2s.com
